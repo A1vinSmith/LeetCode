8c64fd5a9f9083f8304c1116eca538b9fceb0ae0
foyangyu <yanghaotian07@gmail.com>
2017-05-31 17:30:34 +0800

    Init RandomSeq to Main

--- Algorithms/src/com/monster/Main.java
+++ Algorithms/src/com/monster/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World!");
+        System.out.println("Hello Main!");
+        String[] strList = {"5", "100.0", "200.0"};
+
+        RandomSeq.stdout(strList);
     }
 }
---
+++ Algorithms/src/com/monster/RandomSeq.java
@@ -0,0 +1,18 @@
+package com.monster;
+
+/**
+ * Created by sara on 31/05/2017.
+ */
+
+public class RandomSeq {
+    public static void stdout(String[] args) {
+        int N = Integer.parseInt(args[0]);
+        double lo = Double.parseDouble(args[1]);
+        double hi = Double.parseDouble(args[2]);
+        for (int i = 0; i < N; i++) {
+            double x = StdRandom.uniform(lo, hi);
+            StdOut.printf("%.2f\n", x);
+        }
+    }
+}
+
