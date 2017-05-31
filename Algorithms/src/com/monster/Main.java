4df51f3d66ebf37299adc17220f7c1a3f3502b61
foyangyu <yanghaotian07@gmail.com>
2017-05-31 17:23:19 +0800

    Init

--- Algorithms/src/com/monster/Main.java
+++ Algorithms/src/com/monster/Main.java
@@ -3,9 +3,7 @@ package com.monster;
 public class Main {
 
     public static void main(String[] args) {
-
-        String[] strList = {"5", "100.0", "200.0"};
-
-        RandomSeq.stdout(strList);
+	// write your code here
+        System.out.println("Hello World!");
     }
 }
