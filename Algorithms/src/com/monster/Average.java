6ca464dcff4067e82a82647c4d522287672489e0
foyangyu <yanghaotian07@gmail.com>
2017-05-31 17:55:58 +0800

    Add Average

---
+++ Algorithms/src/com/monster/Average.java
@@ -0,0 +1,17 @@
+package com.monster;
+
+/**
+ * Created by sara on 31/05/2017.
+ */
+public class Average {
+    public static void main(String[] args) {
+        double sum = 0.0;
+        int cnt = 0;
+        while (!StdIn.isEmpty()) {
+            sum += StdIn.readDouble();
+            cnt++;
+        }
+        double avg = sum / cnt;
+        StdOut.printf("Average is %.5f\n", avg);
+    }
+}
