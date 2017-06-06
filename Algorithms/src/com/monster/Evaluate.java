a6855fbf2ad91026532458d32eb2db99ced9bbf7
foyangyu <yanghaotian07@gmail.com>
2017-06-06 10:55:52 +0800

    Add Stack.java

---
+++ Algorithms/src/com/monster/Evaluate.java
@@ -0,0 +1,24 @@
+package com.monster;
+
+
+public class Evaluate {
+    public static void main(String[] args) {
+        Stack<String> ops = new Stack<>();
+        Stack<Double> vals = new Stack<>();
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            if (s.equals("("));
+            else if (s.equals("+")) ops.push(s);
+            else if (s.equals("-")) ops.push(s);
+            else if (s.equals("*")) ops.push(s);
+            else if (s.equals("/")) ops.push(s);
+            else if (s.equals("sqrt")) ops.push(s);
+            else if (s.equals(")")) {
+                // If the char is ), pop then calculate push result
+                String op = ops.pop();
+                double v = vals.pop();
+            }
+            else vals.push(Double.parseDouble(s));
+        }
+    }
+}
