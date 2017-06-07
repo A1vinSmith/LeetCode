f57ffee488b84c5e063d4afd077aa50a350c93d0
foyangyu <yanghaotian07@gmail.com>
2017-06-07 16:36:50 +0800

    Add ResizingArrayStack unfinished

---
+++ Algorithms/src/com/monster/ResizingArrayStack.java
@@ -0,0 +1,21 @@
+package com.monster;
+
+/**
+ * Created by sara on 07/06/2017.
+ */
+
+import java.util.Iterator;
+/*
+public class ResizingArrayStack<Item> implements Iterable<Item> {
+    public Iterator<Item> iterator() {
+        return new ReverseArrayIterator();
+    }
+    private class ReverseArrayIterator implements Iterator<Item> {
+        // support last in first out
+        private int i = N;
+        public boolean hasNext() { return i > 0; }
+        public Item next() { return a[--i]; }
+        public void remove() {}
+    }
+}
+*/
