f4f2a00b045fae1cc31a79ddd4172b29f4212ce8
foyangyu <yanghaotian07@gmail.com>
2017-07-25 14:10:48 +0800

    fix: thisCall.js

--- JavaScript/thisCall.js
+++ JavaScript/thisCall.js
@@ -1,28 +1,28 @@
-var fullname = 'John Doe';
-var obj = {
-   fullname: 'Colin Ihrig',
+var fullName = 'John Doe';  // use let, would console undefined in console.log(test());
+let obj = {
+   fullName: 'Colin Ihrig',
    prop: {
-      fullname: 'Aurelio De Rosa',
-      getFullname: function() {
-         return this.fullname;
+      fullName: 'Aurelio De Rosa',
+      getFullName: function() {
+         return this.fullName;
       }
    }
 };
 
-console.log(obj.prop.getFullname());
+console.log(obj.prop.getFullName());
 
-var test = obj.prop.getFullname;
-
+let test = obj.prop.getFullName;
 console.log(test());
+
 console.log(test.call(obj.prop));
 console.log(test.apply(obj.prop));
 /*
 打印出Aurelio De Rosa和John Doe
 原因是在 JavaScript 中，一个函数的上下文环境，也就是this关键词所引用对象，是依赖于函数是如何被调用的，而不是依赖于函数如何b被定义的
 
-在第一个console.log()调用中， getFullname()是作为obj.prop的函数被调用的
-因此，这里的上下文环境指向后者并且函数返回this对象的 fullname属性
-相反，当 getFullname() 被赋为test变量的值时，那个语境指向全局对象(window)
+在第一个console.log()调用中， getFullName()是作为obj.prop的函数被调用的
+因此，这里的上下文环境指向后者并且函数返回this对象的 fullName 属性
+相反，当 getFullName() 被赋为test变量的值时，那个语境指向全局对象(window)
 这是因为，test被隐式设置为全局对象的属性
-因此，函数调用返回window的fullname属性值，在此段代码中，这个值是通过第一行赋值语句设置的
+因此，函数调用返回window的fullName属性值，在此段代码中，这个值是通过第一行赋值语句设置的
 */
