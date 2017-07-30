039825d9bc71321752a59b1665e3e70a484884f6
foyangyu <yanghaotian07@gmail.com>
2017-07-30 21:40:25 +0800

    add: redefArg.js

---
+++ JavaScript/redefinitionArguments.js
@@ -0,0 +1,12 @@
+/**
+ * Created by sara on 30/07/2017.
+ */
+function f(x) {
+    //'use strict';
+    var arguments = []; }
+//undefined
+function f1(x) {
+    'use strict';
+    // Redefining of arguments is not allowed
+    var arguments = [];
+}
