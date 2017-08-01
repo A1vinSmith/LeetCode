bcf8853acbe84d40a7e107ead2ee34d9b8a286a1
foyangyu <yanghaotian07@gmail.com>
2017-08-01 16:42:26 +0800

    add: setIntervalDiv.js

---
+++ JavaScript/setIntervalDiv.js
@@ -0,0 +1,17 @@
+function interval(func, w, t){
+    var interv = function(){
+        if(typeof t === "undefined" || t-- > 0){
+            setTimeout(interv, w);
+            try{
+                func.call(null);
+            }
+            catch(e){
+                t = 0;
+                throw e.toString();
+            }
+        }
+    };
+
+    setTimeout(interv, w);
+}
+
