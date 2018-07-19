ce827553b866c96e881f3f53a8064e983ff3b008
foyangyu <yanghaotian07@gmail.com>
2018-07-19 17:50:29 +0800

    Got it

--- Python/LeetCode/RegExMatching.py
+++ Python/LeetCode/RegExMatching.py
@@ -1,13 +1,15 @@
 class Solution(object):
     def isMatch(self, text, pattern):
-        # Recursive !
+        # Return True, only both none
         if not pattern:
             return not text
-        # First match
+        # Check text exist first, then get ZERO index
         first_match = bool(text) and pattern[0] in {text[0], '.'}
 
+        # When the second index is '*'
         if len(pattern) >= 2 and pattern[1] == '*':
-            return (self.isMatch(text, pattern[2:]) or
-                    first_match and self.isMatch(text[1:], pattern))
+            # Run direction: <--, and then or
+            return self.isMatch(text, pattern[2:]) or first_match and self.isMatch(text[1:], pattern)
         else:
+            # Continue to normal, '*' not found
             return first_match and self.isMatch(text[1:], pattern[1:])
